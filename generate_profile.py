be6c10b87c54b06e4e5af7dab0ddf6f88e7af903
Tommaso <tommaso@example.com>
2025-02-27 23:05:52 +0100

    add soome profiles

--- generate_profile.py
+++ generate_profile.py
@@ -73,7 +73,7 @@ def test_path_gen(motion_profile_path):
     sim.run(1)
 
     save_trajectory_groves_csv(sim, os.path.join(output_dir, "Profile.csv"))
-    sim.plot(['ref_pos', 'ref_att_euler'], opt={'ref_pos': '3d'})
+    sim.plot(['ref_pos', 'ref_vel', 'ref_att_euler'], opt={'ref_pos': '3d'})
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
